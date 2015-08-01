6ff2453d3a146130f21cb8861c13ed590df770bb
test201508 <hanywelcome@yahoo.com>
2015-08-01 20:09:30 +0200

    Update Form1.vb

--- test01/Form1.vb
+++ test01/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("hello all")
+        MsgBox("hello all, This is changed outside your env")
     End Sub
 End Class
